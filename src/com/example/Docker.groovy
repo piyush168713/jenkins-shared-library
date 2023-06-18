e77a434d95506cb9960939ed54c9c7e50c95bfdc
piyush168713 <20BCS9107@cuchd.in>
2023-06-18 16:43:04 +0530

    separate-class

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -12,10 +12,16 @@ class Docker implements Serializable {
 
     def buildDockerImage(String imageName){
         script.echo "building the docker image..."
+        script.sh "docker build -t $imageName ."
+    }
+
+    def dockerLogin(){
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            script.sh "docker build -t $imageName ."
             script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
-            script.sh "docker push $imageName"
         }
+    }
+
+    def dockerPush(String imageName){
+        script.sh "docker push $imageName"
     }
 }
