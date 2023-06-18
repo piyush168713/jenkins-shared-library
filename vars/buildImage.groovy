f85b8e6d60d82c5738ee1b1f9ee49521541e5baf
piyush168713 <20BCS9107@cuchd.in>
2023-06-18 15:27:22 +0530

    groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,23 @@
 #!/usr/bin/env groovy
 
-def call(){
+// def call(){
+//     echo "building the docker image..."
+//     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//         sh 'docker build -t piyush168713/demo-app:jma-2.6 .'
+//         sh "echo $PASS | docker login -u $USER --password-stdin"
+//         sh 'docker push piyush168713/demo-app:jma-2.6'
+//     }
+// }
+
+// use double quotes("") if any variable exists in statement
+
+
+def call(String imageName){
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t piyush168713/demo-app:jma-2.6 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push piyush168713/demo-app:jma-2.6'
+        sh "docker push $imageName"
     }
 }
+
